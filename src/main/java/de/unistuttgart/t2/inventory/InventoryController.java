2a8c1a6ce84421ce0976ca322d7cc1f625ad925c
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2021-10-13 16:30:32 +0200

    added swagger annotations

--- src/main/java/de/unistuttgart/t2/inventory/InventoryController.java
+++ src/main/java/de/unistuttgart/t2/inventory/InventoryController.java
@@ -10,6 +10,7 @@ import de.unistuttgart.t2.common.Product;
 import de.unistuttgart.t2.common.ReservationRequest;
 import de.unistuttgart.t2.inventory.repository.DataGenerator;
 import de.unistuttgart.t2.inventory.repository.InventoryItem;
+import io.swagger.v3.oas.annotations.Operation;
 
 /**
  * 
@@ -22,43 +23,51 @@ import de.unistuttgart.t2.inventory.repository.InventoryItem;
  */
 @RestController
 public class InventoryController {
-	
-	private InventoryService inventoryService;	
-	private DataGenerator generator;
-	
-	public InventoryController(@Autowired InventoryService inventoryService, @Autowired DataGenerator generator) {
-	    assert(generator != null && inventoryService != null);
+
+    private InventoryService inventoryService;
+    private DataGenerator generator;
+
+    public InventoryController(@Autowired InventoryService inventoryService, @Autowired DataGenerator generator) {
+        assert (generator != null && inventoryService != null);
         this.inventoryService = inventoryService;
         this.generator = generator;
     }
 
-	/**
-	 * add a reservation to a product.
-	 * 
-	 * @param body request body
-	 * @return the product that the reservation was added to.
-	 */
-	@PostMapping("/inventory/reservation")
-	public Product addReservation(@RequestBody ReservationRequest body){
-		InventoryItem item = inventoryService.makeReservation(body.getProductId(), body.getSessionId(), body.getUnits());
-		return new Product(item.getId(),item.getName(), item.getDescription(), item.getAvailableUnits(),item.getPrice());
-	}
-	
-	/**
-	 * trigger generation of new products
-	 */
-	@GetMapping("/generate")
-	public void generateData() {
-		generator.generateProducts();
-	}
-	
-	/**
-	 * trigger restock of all products 
-	 */
-	@GetMapping("/restock")
-	public void restock() {
-		generator.restockProducts();
-	}
-	
-	// TODO : exception handler
+    /**
+     * add a reservation to a product.
+     * 
+     * @param body request body
+     * @return the product that the reservation was added to.
+     */
+    @Operation(summary = "Place a reservation", description = "Place a reservation of a number of units for a certain item for a certain user.")
+
+    @PostMapping("/inventory/reservation")
+    public Product addReservation(@RequestBody ReservationRequest body) {
+        InventoryItem item = inventoryService.makeReservation(body.getProductId(), body.getSessionId(),
+                body.getUnits());
+        return new Product(item.getId(), item.getName(), item.getDescription(), item.getAvailableUnits(),
+                item.getPrice());
+    }
+
+    /**
+     * trigger generation of new products
+     * 
+     * TODO post x generation request seems more reasonable
+     */
+    @Operation(summary = "Populate the store with new products", description = "Populate the store with new products")
+    @GetMapping("/generate")
+    public void generateData() {
+        generator.generateProducts();
+    }
+
+    /**
+     * trigger restock of all products
+     * 
+     * TODO post x restock request seems more reasonable
+     */
+    @Operation(summary = "Restock units of the store's products", description = "Restock units of the store's products")
+    @GetMapping("/restock")
+    public void restock() {
+        generator.restockProducts();
+    }
 }
